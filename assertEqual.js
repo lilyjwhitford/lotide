95947d12ad0f2044e90e57c5b1fb27a8340f1de4
Lily Whitford <lilywhitford2@gmail.com>
2024-01-26 00:41:41 +0000

    changed code to use template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,12 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed:" + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
-assertEqual(10, 10);
+assertEqual("cat", "cat");
+assertEqual(100, 101);
