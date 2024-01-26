8b36950d19b85652571da63cda7f10c42bc10532
Lily Whitford <lilywhitford2@gmail.com>
2024-01-26 04:55:06 +0000

    fixed up code

--- tail.js
+++ tail.js
@@ -7,11 +7,7 @@ const assertEqual = function(actual, expected) {
 };
 
 const tail = function(array) {
-  if (array && array.length > 1) { // checking if array has more than 1 element
-    return array.slice(1); // create new array starting from second element
-  } else {
-    return []; // 1 || no elements = empty array
-  }
+  return array.slice(1); // create new array starting from second element
   
 };
 
@@ -20,5 +16,7 @@ tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
 
 const empty = [];
-console.log(tail(empty));
+assertEqual(empty.length);
 
+const example = ["Hi"];
+assertEqual(example.length, 1);
