9b015f7c8b5e8571b64ee0890a479dd6e3f7e266
Lily Whitford <lilywhitford2@gmail.com>
2024-01-26 05:09:32 +0000

    one last test case change

--- tail.js
+++ tail.js
@@ -12,8 +12,8 @@ const tail = function(array) {
 };
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(words.length, 3); 
+assertEqual(tail(words)[0], "Lighthouse");
 
 const empty = [];
 const emptyResult = tail(empty);
