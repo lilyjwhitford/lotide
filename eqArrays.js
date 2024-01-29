935b031b1a39f78e87219a00da4b9ed51b289f5b
Lily Whitford <lilywhitford2@gmail.com>
2024-01-29 01:55:44 +0000

    implemented eqArrays function and used assertEqual to write a couple test cases

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) { // check if arrays have the same length
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) { //loop through and compare individual elements of the arrays
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+return true;
+};
+
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays(["Hi", "there"], ["Hi", "there"]), true);
