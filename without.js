f1203e6432fb769053faff4d9215f61af4f292c6
Lily Whitford <lilywhitford2@gmail.com>
2024-01-29 03:50:13 +0000

    implemented without function

---
+++ without.js
@@ -0,0 +1,35 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  const result = [];
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      result.push(source[i]);
+    }
+  }
+  return result;
+};
+
+const words = ["hello", "world", "lighthouse"];
+const result = without(words, ["lighthouse"]); // => [2, 3]
+
+without(words, ["lighthouse"]); 
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
