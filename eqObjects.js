8306eba958a7e0a49f9d499217c33c6f61abf62b
Lily Whitford <lilywhitford2@gmail.com>
2024-02-03 00:58:56 +0000

    looped through keys and return false if key values dont match

--- eqObjects.js
+++ eqObjects.js
@@ -1,14 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) { // check if arrays have the same length
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) { //loop through and compare individual elements of the arrays
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
@@ -25,13 +14,10 @@ const eqObjects = function(object1, object2) {
   if (keys1.length !== keys2.length) { // compare number of keys in each object
     return false;
   }
+  for (const key of keys1) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
 };
-
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject= { size: "medium", color: "red" };
-console.log(eqObjects(shirtObject , anotherShirtObject)); 
-assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
-
-const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
-eqObjects(shirtObject , longSleeveShirtObject); 
-assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false);
