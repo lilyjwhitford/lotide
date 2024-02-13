d3e1539ae8c069a2dc49a0534f0e76f5dbfd297e
Lily Whitford <lilywhitford2@gmail.com>
2024-02-13 23:45:02 +0000

    exported module/moved test code into new file

--- tail.js
+++ tail.js
@@ -1,24 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   return array.slice(1); // create new array starting from second element
   
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-assertEqual(words.length, 3); 
-assertEqual(tail(words)[0], "Lighthouse");
-
-const empty = [];
-const emptyResult = tail(empty);
-assertEqual(emptyResult.length, 0); 
-
-const example = ["Hi"];
-const exampleResult = tail(example);
-assertEqual(exampleResult.length, 0); 
+module.exports = tail;
