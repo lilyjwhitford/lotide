faceada70f2a9afa8e62abd503c5a096b9d9d540
Lily Whitford <lilywhitford2@gmail.com>
2024-02-13 23:49:08 +0000

    added a test that checks if the length of the array returned by tail is one less than the length of the input array

--- test/tailTest.js
+++ test/tailTest.js
@@ -5,6 +5,7 @@ const tail = require("../tail");
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 assertEqual(words.length, 3); 
 assertEqual(tail(words)[0], "Lighthouse");
+assertEqual(tail(words).length, words.length - 1);
 
 const empty = [];
 const emptyResult = tail(empty);
@@ -13,3 +14,4 @@ assertEqual(emptyResult.length, 0);
 const example = ["Hi"];
 const exampleResult = tail(example);
 assertEqual(exampleResult.length, 0); 
+
