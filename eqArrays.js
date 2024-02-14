02ddd8c6d869ac65223a41a40091ea1585501473
Lily Whitford <lilywhitford2@gmail.com>
2024-02-14 00:02:13 +0000

    exported eqArrays function and removed assertEqual func

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) { // check if arrays have the same length
     return false;
@@ -18,7 +10,5 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
+module.exports = eqArrays;
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays(["Hi", "there"], ["Hi", "there"]), true);
