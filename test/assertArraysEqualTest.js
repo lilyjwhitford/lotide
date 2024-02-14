39fe1730e2be0f29e080fd47dff8c13f1bb7a4d2
Lily Whitford <lilywhitford2@gmail.com>
2024-02-14 00:26:35 +0000

    added some inline commentary

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,7 +1,7 @@
 const assertArraysEqual = require("../assertArraysEqual");
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual([1, 2], [1, 2, 3]);
-assertArraysEqual([[1], [2]], [[1], [2]]);
-assertArraysEqual([], []);
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => should FAIL
+assertArraysEqual([1, 2], [1, 2, 3]); // => should FAIL
+assertArraysEqual([[1], [2]], [[1], [2]]); // => should FAIL
+assertArraysEqual([], []); // => should PASS
