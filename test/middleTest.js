17604c4cac5e9510496022008719754ad9553ed3
Lily Whitford <lilywhitford2@gmail.com>
2024-02-14 00:38:52 +0000

    implemented assertArraysEqual in test cases

--- test/middleTest.js
+++ test/middleTest.js
@@ -2,9 +2,9 @@ const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
 
 
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+assertArraysEqual(middle([1]), []); // => [] should PASS
+assertArraysEqual(middle([1, 2]), []); // => [] should PASS
+assertArraysEqual(middle([1, 2, 3]), [2]); // => [2] should PASS
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3] should PASS
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3] should PASS
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4] should PASS
