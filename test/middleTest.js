de967b792f114291d178f41bbeefe592ec212e82
Lily Whitford <lilywhitford2@gmail.com>
2024-02-14 01:49:57 +0000

    incorporated m & c

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,18 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require('chai').assert;
 const middle = require("../middle");
 
+describe("#middle", () => {
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
 
-assertArraysEqual(middle([1]), []); // => [] should PASS
-assertArraysEqual(middle([1, 2]), []); // => [] should PASS
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2] should PASS
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3] should PASS
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3] should PASS
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4] should PASS
+});
